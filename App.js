e86cbe99599e9305ee0c43ced2706276edbaf411
Kurt Tamulonis <kurttamulonis@gmail.com>
2017-12-05 15:47:33 -0500

    changed front view

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{color: 'red'}}>thought
+          <Text style={{color: 'white'}}>sort</Text>
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
